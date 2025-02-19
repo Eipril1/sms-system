028f8117f4fea645a69e05ee80bca13c08fd525a
Eipril1 <879989023@qq.com>
2025-02-19 12:06:23 +0800

    2.19提交测试v1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '',  // 使用空字符串
+    base: '/',  // 改回使用根路径
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
